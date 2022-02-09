31f6c47160475a63101be23ffa7a0697fb17d51b
lijincheng <1911159016@qq.com>
2022-02-10 02:18:57 +0800

    引入js引擎(测试)

--- SlimeJumping/src/mamager/GameManager.cs
+++ SlimeJumping/src/mamager/GameManager.cs
@@ -1,4 +1,6 @@
 using Godot;
+using Calljs;
+using System;
 
 /// <summary>
 /// 游戏管理器, 负责管理整个项目其它的Manager, 并更新它们
@@ -20,7 +22,27 @@ public class GameManager : Node
 			return;
 		}
 		_inited = true;
-	}
+
+		try
+        {
+			ScriptManager.Out = new JsLog();
+			IScriptSerivce service = new ClearScriptService();
+			ScriptManager.Register(service);
+
+			service.ScanJsClass(typeof(GameManager).Assembly);
+			//service.AddHostInstance(new HostInstance("console.log", new LogMethod(ScriptManager.Out.Log)));
+			//service.AddHostInstance(new HostInstance("console.error", new LogMethod(ScriptManager.Out.LogError)));
+			//service.Evaluate("console.log('666')");
+			service.Execute("var a = 1; a += 2;");
+		}
+		catch (Exception e)
+        {
+			//GD.PrintErr(e.Message);
+			//GD.PrintErr(e.StackTrace);
+        }
+
+
+    }
 
 	public override void _Process(float delta)
 	{
