825731383be86261d790d1c104b44042121a505c
lijincheng <1911159016@qq.com>
2022-02-11 02:20:27 +0800

    引用本地库

--- SlimeJumping/src/mamager/GameManager.cs
+++ SlimeJumping/src/mamager/GameManager.cs
@@ -23,26 +23,38 @@ public class GameManager : Node
 		}
 		_inited = true;
 
+		
+		
+
 		try
         {
-			ScriptManager.Out = new JsLog();
+			GD.Print("000000000");
+			GD.Print("哈哈哈哈1313++98+123#@!#$");
+			//ScriptManager.Out = new JsLog();
 			IScriptSerivce service = new ClearScriptService();
+			GD.Print("00000001");
 			ScriptManager.Register(service);
-
+			Console.WriteLine("12345");
+			GD.Print("00000002");
 			service.ScanJsClass(typeof(GameManager).Assembly);
-			//service.AddHostInstance(new HostInstance("console.log", new LogMethod(ScriptManager.Out.Log)));
+            //service.AddHostInstance(new HostInstance("console.log", new LogMethod(ScriptManager.Out.Log)));
 			//service.AddHostInstance(new HostInstance("console.error", new LogMethod(ScriptManager.Out.LogError)));
 			//service.Evaluate("console.log('666')");
-			service.Execute("var a = 1; a += 2;");
-		}
+			GD.Print("111111");
+			var data = service.Evaluate("1 + 1").Object;
+			GD.Print("222222");
+			GD.Print(data);
+			GD.Print("333333");
+            //service.Execute("var a = 1; a += 2;");
+        }
 		catch (Exception e)
         {
-			//GD.PrintErr(e.Message);
-			//GD.PrintErr(e.StackTrace);
+			GD.Print(e.Message);
+			GD.Print("有可能有中文路径...");
+			GD.Print("555555");
         }
-
-
-    }
+		GD.Print("6666666");
+	}
 
 	public override void _Process(float delta)
 	{
