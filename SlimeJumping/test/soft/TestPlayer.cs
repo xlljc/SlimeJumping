701b157506aa854ab5ef53b6bfc4489763a574ea
小李xl <1911159016@qq.com>
2022-03-31 02:23:36 +0800

    玩家形变

--- SlimeJumping/test/soft/TestPlayer.cs
+++ SlimeJumping/test/soft/TestPlayer.cs
@@ -5,13 +5,18 @@ public class TestPlayer : RigidBody2D
     public override void _PhysicsProcess(float delta)
     {
         base._PhysicsProcess(delta);
-        for (var i = 1; i < 4; i++)
+        for (var i = 0; i <= 4; i++)
         {
-            // GetNode<Bone2D>("Skeleton2D/" + i.ToString()).GlobalPosition = GetNode<RigidBody2D>(i.ToString()).GlobalPosition;
-            // if (i > 0)
-            // {
-            //     GetNode<Bone2D>("Skeleton2D/" + i.ToString()).LookAt(GetNode<Node2D>("0").GlobalPosition);
-            // }
+            Bone2D bone = GetNode<Bone2D>("Skeleton2D/" + i.ToString());
+            if (i > 0)
+            {
+                bone.LookAt(GlobalPosition);
+                bone.GlobalPosition = GetNode<RigidBody2D>(i.ToString()).GlobalPosition;
+            }
+            else
+            {
+                bone.GlobalPosition = GlobalPosition;
+            }
         }
     }
 }
